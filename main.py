9f00918da9e2bf2b08e4644d4d89d20b08b1c1c0
Xisurthros <Jacob_Hobbs548@yahoo.com>
2022-08-13 19:29:53 -0500

    Clear MAC Address

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from wakeonlan import send_magic_packet
 print("Attempting to connect")
 ser = serial.Serial("/dev/ttyACM0", 9600)
 print("connected")
-IDs = ["63 A0 58 93", "4C DC 64 49"]
+IDs = ["63 A0 58 93", "4C DC 64 49"] # Change to the IDs you want to allow
 
 while True:
 	try:
 		code = ser.readline().decode('utf-8')[1:12]
 		print(code)
 		if code in IDs:
-			send_magic_packet('4C.CC.6A.2B.BA.A8')
+			send_magic_packet('YOUR_DEVICE_MAC_ADRESS') # Set to your devices MAC address
 			print("TURNING ON!")
 	except KeyboardInterrupt:
 		break
